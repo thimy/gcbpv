3cdac57529b2f49d3ffb4bb3a8fee38a8be27dc5
Hugo Fouques <hugo.fouques@gmail.com>
2024-04-14 13:57:48 +0200

    Add instrument scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope :secretariat do
+    resources :instruments
     get "", to: "secretariat#index", as: "/"
   end  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ test/system/instruments_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class InstrumentsTest < ApplicationSystemTestCase
+  setup do
+    @instrument = instruments(:one)
+  end
+
+  test "visiting the index" do
+    visit instruments_url
+    assert_selector "h1", text: "Instruments"
+  end
+
+  test "should create instrument" do
+    visit instruments_url
+    click_on "New instrument"
+
+    fill_in "Description", with: @instrument.description
+    fill_in "Name", with: @instrument.name
+    click_on "Create Instrument"
+
+    assert_text "Instrument was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Instrument" do
+    visit instrument_url(@instrument)
+    click_on "Edit this instrument", match: :first
+
+    fill_in "Description", with: @instrument.description
+    fill_in "Name", with: @instrument.name
+    click_on "Update Instrument"
+
+    assert_text "Instrument was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Instrument" do
+    visit instrument_url(@instrument)
+    click_on "Destroy this instrument", match: :first
+
+    assert_text "Instrument was successfully destroyed"
+  end
+end
