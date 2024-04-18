94bc29d95d9945baec63650793e919132e18c912
Thimy Kieu <thimy@bump.sh>
2024-04-18 21:51:58 +0200

    home hero

--- app/components/website/hero_card/component.rb
+++ app/components/website/hero_card/component.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class Shared::Card::Component < ViewComponent::Base
+class Website::HeroCard::Component < ViewComponent::Base
   def initialize(title:, url:, css_classes: "")
     @title = title
     @url = url
-    @css_classes = "card #{css_classes}"
+    @css_classes = "hero-card #{css_classes}"
   end
 end
