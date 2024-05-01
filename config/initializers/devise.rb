37ee2a1c9553f8fdd2277ece1212f707e15a4bea
Thimy Kieu <thimy@bump.sh>
2024-05-01 12:27:04 +0200

    install letter_opener

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,4 +311,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.mailer_sender = 'ne-pas-repondre@gcbpv.org'
 end
