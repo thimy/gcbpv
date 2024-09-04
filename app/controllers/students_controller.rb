fc875750f2cf386b80afa54c856f7db0244af9f0
Thimy Kieu <thimy@bump.sh>
2024-09-04 21:48:11 +0200

    fix subscription

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,4 @@
-class Secretariat::StudentsController < SecretariatController
+class StudentsController < SecretariatController
   before_action :set_student, only: %i[ show edit update destroy ]
 
   # GET /students or /students.json
