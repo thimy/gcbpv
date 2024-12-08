dfcb80c532132d850cda691321fad935ea109283
Thimy Kieu <thimy@bump.sh>
2024-12-08 13:09:03 +0100

    make payment amount required

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,7 @@
 class Payment < ApplicationRecord
   belongs_to :subscription_group
 
+  validates :amount, presence: true
+
   enum :payment_method, "Espèces" => 0, "Chèque" => 1, "Virement" => 2, "Chèques Vacances" => 3, "Coupon Culture Sport Redon" => 4, "Forfait Passion CAF" => 5, "Chèques Vacances Connect" => 6
 end
