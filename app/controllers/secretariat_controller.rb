86219693e47129e442669e89f5a4c412db644b7a
Thimy Kieu <thimy@bump.sh>
2025-06-16 10:25:44 +0200

    fix season select

--- app/controllers/secretariat_controller.rb
+++ app/controllers/secretariat_controller.rb
@@ -25,6 +25,6 @@ class SecretariatController < ApplicationController
 
   def set_season
     @season = params[:season_name].present? ? Season.find_by(start_year: params[:season_name].split("-").first) : Config.first.season
-    @seasons = Season.all.map {|season| {name: season.name, link: request.path.gsub!(@season.name, season.name)}}
+    @season_links = Season.all.map {|season| {name: season.name, link: request.path.gsub!(@season.name, season.name)}}
   end
 end
