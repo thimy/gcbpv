7117d3b32bcfe168c773905a25f62b91632991cc
Thimy Kieu <thimy@bump.sh>
2025-07-03 20:20:43 +0200

    cleaning

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -160,159 +160,55 @@ class SubscriptionsController < SecretariatController
 
   private
 
-    def query
-      params[:q]
+  def query
+    params[:q]
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_subscription
+    @subscription = Subscription.find(params[:id] || params[:subscription_id])
+    @subscription_group = @subscription.subscription_group
+  end
+
+  def set_cities
+    @cities = Student.all.map { |student|
+      student.city_or_payor_city
+    }.flatten.uniq.reject { |c| c.blank? }.sort
+  end
+
+  # Only allow a list of trusted parameters through.
+  def subscription_params
+    if params[:payor_address] == "yes"
+      params[:subscription][:student_attributes][:address] = nil
+      params[:subscription][:student_attributes][:postcode] = nil
+      params[:subscription][:student_attributes][:city] = nil
     end
+    params.require(:subscription).permit(:student_id, :subscription_group_id, :ars, :disability, :image_consent, :comment, courses_attributes: [:instrument_id, :slot_id, :option, :comment], subbed_workshops_attributes: [:workshop_slot_id, :option, :comment], subbed_kid_workshops_attributes: [:kid_workshop_slot_id, :option, :comment], subbed_kid_workshop_attributes: [:kid_workshop_slot_id, :option, :comment], student_attributes: [:id, :first_name, :last_name, :phone, :email, :gender, :address, :postcode, :city, :birth_year, :comment])
+  end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_subscription
-      @subscription = Subscription.find(params[:id] || params[:subscription_id])
-      @subscription_group = @subscription.subscription_group
+  def subbed_kid_workshop_params
+    if params[:subscription][:subbed_kid_workshop].present?
+      params[:subscription][:subbed_kid_workshop][:option] = params[:subscription][:subbed_kid_workshop][:option].to_i
     end
+    params[:subscription].require(:subbed_kid_workshop).permit(:sub_id, :kid_workshop_slot_id, :option, :comment)
+  end
 
-    def set_cities
-      @cities = Student.all.map { |student|
-        student.city_or_payor_city
-      }.flatten.uniq.reject { |c| c.blank? }.sort
+  def course_params
+    if params[:subscription][:course].present?
+      params[:subscription][:course][:option] = params[:subscription][:course][:option].to_i
     end
+    params[:subscription].require(:course).permit(:instrument_id, :slot_id, :option, :comment)
+  end
 
-    # Only allow a list of trusted parameters through.
-    def subscription_params
-      if params[:payor_address] == "yes"
-        params[:subscription][:student_attributes][:address] = nil
-        params[:subscription][:student_attributes][:postcode] = nil
-        params[:subscription][:student_attributes][:city] = nil
-      end
-      params.require(:subscription).permit(:student_id, :subscription_group_id, :ars, :disability, :image_consent, :comment, courses_attributes: [:instrument_id, :slot_id, :option, :comment], subbed_workshops_attributes: [:workshop_slot_id, :option, :comment], subbed_kid_workshops_attributes: [:kid_workshop_slot_id, :option, :comment], subbed_kid_workshop_attributes: [:kid_workshop_slot_id, :option, :comment], student_attributes: [:id, :first_name, :last_name, :phone, :email, :gender, :address, :postcode, :city, :birth_year, :comment])
+  def subbed_workshop_params
+    if params[:subscription][:subbed_workshop].present?
+      params[:subscription][:subbed_workshop][:option] = params[:subscription][:subbed_workshop][:option].to_i
     end
+    params[:subscription].require(:subbed_workshop).permit(:workshop_slot_id, :option, :comment)
+  end
 
-    def subbed_kid_workshop_params
-      if params[:subscription][:subbed_kid_workshop].present?
-        params[:subscription][:subbed_kid_workshop][:option] = params[:subscription][:subbed_kid_workshop][:option].to_i
-      end
-      params[:subscription].require(:subbed_kid_workshop).permit(:sub_id, :kid_workshop_slot_id, :option, :comment)
-    end
-
-    def course_params
-      if params[:subscription][:course].present?
-        params[:subscription][:course][:option] = params[:subscription][:course][:option].to_i
-      end
-      params[:subscription].require(:course).permit(:instrument_id, :slot_id, :option, :comment)
-    end
-
-    def subbed_workshop_params
-      if params[:subscription][:subbed_workshop].present?
-        params[:subscription][:subbed_workshop][:option] = params[:subscription][:subbed_workshop][:option].to_i
-      end
-      params[:subscription].require(:subbed_workshop).permit(:workshop_slot_id, :option, :comment)
-    end
-
-    def student_params
-      if params[:student].present?
-        if params[:is_payor] == "1"
-          payor = Payor.find { |payor|
-            params[:payor][:name] == payor.name
-          }
-
-          if payor.present?
-            params[:student][:first_name] = payor.first_name
-            params[:student][:last_name] = payor.last_name
-          else
-            params[:student][:first_name] = payor_params[:first_name]
-            params[:student][:last_name] = payor_params[:last_name]
-          end
-          if params[:student][:birth_year].present?
-            params[:student][:birth_year] = params[:student][:birth_year].to_i
-          end
-        end
-        params.require(:student).permit(:first_name, :last_name, :birth_year, :gender, :phone, :email, :comment, :address, :postcode, :city)
-      end
-    end
-
-    def payor_params
-      if params[:payor].present?
-        params.require(:payor).permit(:first_name, :last_name, :phone, :email, :address, :postcode, :city, :comment)
-      end
-    end
-
-    def set_lists
-      @students = Student.all
-      @payors = Payor.all
-      @instruments = Instrument.active
-      @workshops = Workshop.active
-      @kid_workshops = KidWorkshop.active
-    end
-
-    def set_records
-      @filters = {}
-      @filters[:subscription_group] = {}
-      @filters[:subscription_group][:season] = @season
-
-      @filters[:subscription_group][:status] = params[:status] if params[:status].present?
-      @filters[:subscription_group][:majoration_class] = params[:majoration_class] if params[:majoration_class].present?
-
-      @filters[:subbed_kid_workshops] = SubbedKidWorkshop.includes(:kid_workshop_slot).where(kid_workshop_slot: { kid_workshop: params[:kid_workshop] }) if params[:kid_workshop].present?
-
-      @course_filter = {}
-      @course_filter[:instrument] = params[:instrument] if params[:instrument].present?
-      @course_filter[:slot] = {}
-      @course_filter[:slot][:teacher] = params[:teacher] if params[:teacher].present?
-      @course_filter[:slot][:day_of_week] = params[:course_day] if params[:course_day].present?
-      @course_filter[:slot][:frequency] = params[:frequency] if params[:frequency].present?
-      @filters[:courses] = Course.includes(:slot).where(@course_filter) if @course_filter[:slot].present? || @course_filter[:instrument].present?
-
-      @workshop_filter = {}
-      @workshop_filter[:workshop] = params[:workshop] if params[:workshop].present?
-      @workshop_filter[:day_of_week] = params[:workshop_day] if params[:workshop_day].present?
-      @filters[:subbed_workshops] = SubbedWorkshop.includes(:workshop_slot).where(workshop_slot: @workshop_filter) if @workshop_filter.present?
-
-      @filters[:ars] = params[:ars] if params[:ars].present?
-      @filters[:image_consent] = params[:image_consent] if params[:image_consent].present?
-
-      @not_filters = {}
-      @not_filters[:subscription_group] = { status: [3] }
-      @not_filters[:subscription_group][:status].concat(params[:exclude_status]) if params[:exclude_status].present?
-
-      @filtered_subscriptions = Subscription.includes(:subscription_group).where(@filters).where.not(@not_filters).where(student: Student.where("last_name ILIKE :term OR first_name ILIKE :term OR :term IS NULL", { term: "%#{query}%" }))
-
-      @filtered_subscriptions = @filtered_subscriptions.where(student: Student.where("birth_year > ?", params[:birthyear_under])) if params[:birthyear_under].present?
-      
-      @filtered_subscriptions = @filtered_subscriptions.where(student: Student.where("birth_year < ?", params[:birthyear_over])) if params[:birthyear_over].present?
-      
-      if params[:postcode].present?
-        @filtered_subscriptions = @filtered_subscriptions.where(student: Student.where("postcode LIKE ?", "#{params[:postcode]}%")).or(@filtered_subscriptions.where(student: Student.where(postcode: nil), subscription_group: { payor: Payor.where("postcode LIKE ?", "#{params[:postcode]}%")}))
-      end
-
-      if params[:city].present?
-        @filtered_subscriptions = @filtered_subscriptions.where(student: Student.where("city = ?", params[:city])).or(@filtered_subscriptions.where(student: Student.where(city: nil), subscription_group: { payor: Payor.where("city = ?", params[:city]) }))
-      end
-
-      @item_size = @filtered_subscriptions.size
-      @selected_emails = @filtered_subscriptions.map {|subscription| subscription.email}.uniq.join("\n")
-      @pagy, @subscriptions = paginate_records(@filtered_subscriptions)
-    end
-    
-    def default_sort_attribute
-      SORT_ATTRIBUTES["subscriptions"].first
-    end
-
-    def with_sort_params(records)
-      if @sort_attribute.present?
-        if @sort_attribute == "student"
-          records.includes(:student).merge(Student.order(last_name: @sort_direction))
-        elsif @sort_attribute == "birth_year"
-          records.includes(:student).merge(Student.order(birth_year: @sort_direction))
-        end
-      else
-        records
-      end
-    end
-  
-    def valid_sort_attribute?(attribute)
-      SORT_ATTRIBUTES["subscriptions"].include?(attribute)
-    end
-
-    def build_student
+  def student_params
+    if params[:student].present?
       if params[:is_payor] == "1"
         payor = Payor.find { |payor|
           params[:payor][:name] == payor.name
@@ -329,5 +225,40 @@ class SubscriptionsController < SecretariatController
           params[:student][:birth_year] = params[:student][:birth_year].to_i
         end
       end
+      params.require(:student).permit(:first_name, :last_name, :birth_year, :gender, :phone, :email, :comment, :address, :postcode, :city)
     end
+  end
+
+  def payor_params
+    if params[:payor].present?
+      params.require(:payor).permit(:first_name, :last_name, :phone, :email, :address, :postcode, :city, :comment)
+    end
+  end
+
+  def set_lists
+    @students = Student.all
+    @payors = Payor.all
+    @instruments = Instrument.active
+    @workshops = Workshop.active
+    @kid_workshops = KidWorkshop.active
+  end
+
+  def build_student
+    if params[:is_payor] == "1"
+      payor = Payor.find { |payor|
+        params[:payor][:name] == payor.name
+      }
+
+      if payor.present?
+        params[:student][:first_name] = payor.first_name
+        params[:student][:last_name] = payor.last_name
+      else
+        params[:student][:first_name] = payor_params[:first_name]
+        params[:student][:last_name] = payor_params[:last_name]
+      end
+      if params[:student][:birth_year].present?
+        params[:student][:birth_year] = params[:student][:birth_year].to_i
+      end
+    end
+  end
 end
--- app/views/secretariat/subscriptions/_subscription.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! subscription, :id, :year, :student_id, :course_id, :workshop_id, :paid_amount, :payor_id, :created_at, :updated_at
-json.url subscription_url(subscription, format: :json)
