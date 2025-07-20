bf0e22ac6bacc33830880878d3b2d7db61f410dc
Thimy Kieu <thimy@bump.sh>
2025-07-20 23:38:43 +0200

    improve youth public page

--- app/controllers/ecole/youth_controller.rb
+++ app/controllers/ecole/youth_controller.rb
@@ -4,5 +4,7 @@ class Ecole::YouthController < EcoleController
     @early_learning = KidWorkshop.find_by(name: "Éveil musical")
     @kid_discovery = KidWorkshop.find_by(name: "Découverte instrumentale enfants")
     @kid_workshops = Workshop.active.where(kid_friendly: true)
+    @parent_kid = KidWorkshop.find_by(name: "Éveil petite enfance")
+    @family_pathway = KidWorkshop.find_by(name: "Parcours famille")
   end
 end
