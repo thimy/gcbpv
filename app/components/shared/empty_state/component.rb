4117c2ec4d5196c29b9b07cb9dc3465ed108c8fb
Thimy Kieu <thimy@bump.sh>
2026-03-04 15:51:33 +0100

    add empty states to bogue pages

---
+++ app/components/shared/empty_state/component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Shared::EmptyState::Component < ViewComponent::Base
+end
---
+++ test/components/shared/empty_state/component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Shared::EmptyState::ComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(EmptyStateComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
