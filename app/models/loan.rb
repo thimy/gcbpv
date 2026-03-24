a77aa1c24a74a4d72f60ed46ade84a86fe37ac42
Thimy Kieu <thimy@bump.sh>
2026-03-24 16:49:55 +0100

    remove validation on loan fields

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,7 +1,3 @@
 class Loan < ApplicationRecord
   belongs_to :subscription
-
-  validates :subscription, presence: true
-  validates :instrument, presence: true
-  validates :cost, presence: true
 end
